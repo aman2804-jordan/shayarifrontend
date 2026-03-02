05e7d91b2dbb9e2a3551460a94bf766c1f77b54e
aman2804-jordan <tiwari113@gmail.com>
2026-03-03 04:04:55 +0530

    changes made for mobile view

--- src/components/Layout/UI/Headers.jsx
+++ src/components/Layout/UI/Headers.jsx
@@ -1,37 +1,42 @@
 import { NavLink } from "react-router-dom";
 import { GiFeather } from "react-icons/gi";
+import { useState } from "react";
 
 export const Headers = () => {
-    return (
-         <header>
-            <div className="container">
-                <div className="grid navbar-grid">
-                    <div className="Logo">
-                        <NavLink to="/">
-                        <h1><GiFeather/>AeJazbaaat</h1>
-                        </NavLink>
-                    </div>
-                    <nav>
-                        <ul>
-                            <li>
-                            <NavLink to="/">Home</NavLink>
-                            </li>
-                            <li>
-                            <NavLink to="/about">About</NavLink>
-                            </li>
-                            <li>
-                            <NavLink to="/shayars">Shayars</NavLink>
-                            </li>
-                            <li>
-                            <NavLink to="share">Share</NavLink>
-                            </li>
-                             <li>
-                            <NavLink to="admin">Client</NavLink>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-         </header>
-    );
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header>
+      <div className="container">
+        <div className="navbar-grid">
+
+          <div className="Logo">
+            <NavLink to="/">
+              <h1><GiFeather /> AeJazbaaat</h1>
+            </NavLink>
+          </div>
+
+          {/* Hamburger */}
+          <div
+            className="ham-menu"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            ☰
+          </div>
+
+          <nav className={menuOpen ? "active" : ""}>
+            <ul onClick={() => setMenuOpen(false)}>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+              <li><NavLink to="/shayars">Shayars</NavLink></li>
+              <li><NavLink to="/share">Share</NavLink></li>
+              <li><NavLink to="/admin">Client</NavLink></li>
+            </ul>
+          </nav>
+
+        </div>
+      </div>
+    </header>
+  );
 };
