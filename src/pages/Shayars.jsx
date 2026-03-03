3a22afcb3d620ed0c1e771651f9bbbed9d43a7f0
aman2804-jordan <tiwari113@gmail.com>
2026-03-03 15:02:17 +0530

    loading sign

--- src/pages/Shayars.jsx
+++ src/pages/Shayars.jsx
@@ -1,20 +1,24 @@
 import { useState, useEffect } from 'react';
+import GoldenLoader from "../components/GoldenLoader";
 
 export const Shayars = () => {
   const [shayaris, setShayaris] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   const fetchShayari = async () => {
-    try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/getAllShayari`);
-      if (!response.ok) throw new Error('Failed to fetch shayari');
-      const data = await response.json();
-      const filtered = data.filter(item => item.status !== "PENDING");
-      setShayaris(filtered);
-    } catch (error) {
-      console.error('Error fetching shayari:', error);
-    }
-  };
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/getAllShayari`);
+    if (!response.ok) throw new Error('Failed to fetch shayari');
 
+    const data = await response.json();
+    const filtered = data.filter(item => item.status !== "PENDING");
+    setShayaris(filtered);
+
+  } catch (error) {
+    console.error('Error fetching shayari:', error);
+  } finally {
+    setLoading(false);   // ✅ VERY IMPORTANT
+  }
+}; 
   useEffect(() => {
     fetchShayari();
   }, []);
@@ -25,18 +29,24 @@ export const Shayars = () => {
         Here are The Famous Shayari's By<br/>The Famous Shayar's...
       </h2>
       <div className="gradient-cards">
-        {shayaris.map((shayari) => (
-          <div className="card" key={shayari._id}>
-            <div className="container-card bg-yellow-box">
-              <p>{shayari.shayari}</p>
-              <p>
-                <span className="card-description">By: </span>
-                {shayari.username}
-              </p>
-            </div>
-          </div>
-        ))}
+  {loading ? (
+    <GoldenLoader />
+  ) : shayaris.length === 0 ? (
+    <p style={{ textAlign: "center" }}>No Shayari Found</p>
+  ) : (
+    shayaris.map((shayari) => (
+      <div className="card" key={shayari._id}>
+        <div className="container-card bg-yellow-box">
+          <p>{shayari.shayari}</p>
+          <p>
+            <span className="card-description">By: </span>
+            {shayari.username}
+          </p>
+        </div>
       </div>
+    ))
+  )}
+</div>
     </section>
   );
 };
