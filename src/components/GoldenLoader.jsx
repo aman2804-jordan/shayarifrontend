4d3fdacc473ecf3877802a0e17bed74cd82a1528
aman2804-jordan <tiwari113@gmail.com>
2026-03-03 16:04:19 +0530

    ring

--- src/components/GoldenLoader.jsx
+++ src/components/GoldenLoader.jsx
@@ -3,11 +3,7 @@ import "./GoldenLoader.css";
 const GoldenLoader = () => {
   return (
     <div className="golden-loader-wrapper">
-      <div className="golden-loader">
-        <div className="ring"></div>
-        <div className="ring"></div>
-        <div className="ring"></div>
-      </div>
+      <div className="golden-loader"></div>
       <p className="loading-text">Loading Shayari...</p>
     </div>
   );
