fd08b7e63f5b7feaaac85b3d0a0703cd2b3257e3
aman2804-jordan <tiwari113@gmail.com>
2026-03-07 22:34:49 +0530

    removed import about from homejs file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import { HeroSection } from "../components/Layout/UI/HeroSection";
-import { About } from "./About";
+
 
 export const Home = () => {
     return(
     <>
      <HeroSection />
-    <About />
+    
     </>
     );
 };
